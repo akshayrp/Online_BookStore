3d07f88dd2061213d078e562b69d86cbe26a5468
kumudgarg <kumud50garg@gmail.com>
2020-01-14 17:41:19 +0530

    Added:initial commit of this branch

--- src/main/java/com/thoughtworks/onlinebookstore/controller/OnlineBookShopController.java
+++ src/main/java/com/thoughtworks/onlinebookstore/controller/OnlineBookShopController.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.onlinebookstore.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OnlineBookShopController {
+
+
+    @GetMapping("/list")
+    public String getList() {
+        return "Hello World !";
+    }
 }
