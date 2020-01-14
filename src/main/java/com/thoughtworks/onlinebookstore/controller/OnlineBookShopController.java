e2ea319a00f795424bbe460c1810d863e0490ecd
jitendraB <jitendrabachhav0435@gmail.com>
2020-01-14 17:26:16 +0530

    new branch

--- src/main/java/com/thoughtworks/onlinebookstore/controller/OnlineBookShopController.java
+++ src/main/java/com/thoughtworks/onlinebookstore/controller/OnlineBookShopController.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.onlinebookstore.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OnlineBookShopController {
+
 }
